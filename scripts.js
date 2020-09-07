a7ef401d79319cce4baeff91b370b10b1abfb1eb
knagisa <37279953+knagisa@users.noreply.github.com>
2020-09-07 15:01:35 +0900

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -2,7 +2,9 @@ const display = document.getElementById("display")
 const button = document.getElementById("button")
 
 // const omikuji = ["大吉", "吉", "中吉", "小吉", "末吉", "凶"]
-const omikuji = ["芸術A", "歴史学A", "外国語への招待2", "言語と文化", "法学A", "心理学A", "現代社会の課題とコミュニケーション", "東南アジア", "科学技術倫理A", "社会モデリングA"];
+const omikuji = ["芸術A", "歴史学A", "外国語への招待2", "言語と文化", "哲学A", "文学A", "宗教学A", "人間文化論A", 
+                 "法学A", "心理学A", "現代社会の課題とコミュニケーション", "Southeast Asia", "政治学", "ファッション論", "メディア論", 
+                 "科学技術倫理A", "社会モデリングA", "Thinking and learning through meseums", "科学史A", "技術史A", "科学哲学A", "言語学A", "Technology and art", "未来デザイン入門"];
 
 let omikujiId;
 let i = 0;
